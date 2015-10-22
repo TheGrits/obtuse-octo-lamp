626c4fbd27ec537a12b56167b710450c2e877f06
The_Grits <gritsprogramming@gmail.com>
2015-10-22 14:12:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-
+var dropdown = document.getElementById("languages");
+var selected_lan = dropdown.options[dropdown.selectedIndex].text;
+var appending = "";
+if (selected_lan == English){
+  appending = en;
+} else if (selected_lan == French){
+  appending = fr;
+} else if (selected_lan == Spanish){
+  appending = sp;
+}
+var name = appending + ".txt";
+$('#description').load(name);
